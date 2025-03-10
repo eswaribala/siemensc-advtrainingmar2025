4de6e9c3ecb682962a0fe0801584a093279c341e
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-10 15:22:12 +0530

    updated

--- CustomerAPI/CustomerAPI/Contexts/CustomerContext.cs
+++ CustomerAPI/CustomerAPI/Contexts/CustomerContext.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CustomerAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomerAPI.Contexts
 {
     public class CustomerContext:DbContext
     {
+        public CustomerContext(DbContextOptions<CustomerContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+            Database.EnsureCreated();
+        }
 
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Individual> Individuals { get; set; }
+        public DbSet<Corporate> Corporates { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Individual>()
+                .Property(I => I.Gender).HasConversion<string>()
+                .HasMaxLength(50);
+            modelBuilder.Entity<Corporate>()
+                .Property(C => C.CompanyType).HasConversion<string>()
+                .HasMaxLength(50);
+        }
     }
 }
