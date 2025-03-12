d0faa2ca22f428e86d7de31b2251e359945646c3
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-03-12 15:01:03 +0530

    updated

--- OrderAPI/Models/Order.cs
+++ OrderAPI/Models/Order.cs
@@ -1,13 +1,19 @@
 ﻿namespace OrderAPI.Models
 {
+    public enum OrderStatus
+    {
+        Pending,
+        Processing,
+        Completed,
+        Cancelled
+    }
     public class Order
     {
         public int OrderId { get; set; }
         public string OrderName { get; set; }
         public string OrderDescription { get; set; }
-        public string OrderStatus { get; set; }
-        public string OrderDate { get; set; }
-        public string OrderTime { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public string OrderDate { get; set; }        
         public string OrderAmount { get; set; }
     }
 }
